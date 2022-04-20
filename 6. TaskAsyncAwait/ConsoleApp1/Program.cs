b6de97f8f73a608808b73171ba7ff23027fc9adc
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2022-04-20 01:32:41 -0400

    L6

---
+++ 6. TaskAsyncAwait/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+//var lines = File.ReadAllLines("Textfile1.txt");
+
+//foreach (var line in lines)
+//{
+//    Console.WriteLine(line);
+//}
+
+var task = File.ReadAllLinesAsync("Textfile1.txt")
+    .ContinueWith(t =>
+    {
+        var lines = t.Result;
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    });
+Console.ReadKey();
