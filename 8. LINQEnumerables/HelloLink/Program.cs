d1d9390570437f34037b354aa5b872b1587fdd0a
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2022-04-22 13:55:00 -0400

    Intro to LINQ added

---
+++ 8. LINQEnumerables/HelloLink/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+JsonSerializer.Deserialize<DataMisalignedException>
+
+var even = GenerateNumbers(10).Where(x => { return x % 2 == 0; }).Select(x => x * 3);
+even.Count();
+foreach (var item in even)
+{
+    Console.WriteLine(item);
+}
+
+IEnumerable<int> GenerateNumbers(int maxValue)
+{
+    for (int i = 0; i <= maxValue; i++)
+    {
+        yield return i ;
+    }
+}
